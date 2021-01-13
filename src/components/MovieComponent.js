756f99a20e19ff584e9bdd73ce831a745c08dbaa
aboubacarmsr <aboubacarmsr@gmail.com>
2021-01-13 18:29:57 +0100

    adding framer motion

--- src/components/MovieComponent.js
+++ src/components/MovieComponent.js
@@ -2,17 +2,20 @@ import React from 'react'
 import { fetchCover } from "../api/combinedAPI"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
 const MovieComponent = ({ id, title, rating, release, overview, cover_path}) => {
     const cover = fetchCover(cover_path);
     return ( 
         <Link style={{ textDecoration: "none" }} to={{ pathname: `/movie/${id}` }}>
-            <div className="movie">
+            <motion.div initial={{ opacity: 0, scale: 0}} animate={{ opacity: 1, scale: 1 }} 
+                transition={{ duration: 0.7 }} className="movie">
                 <div className="movie-image">
                     <img src={cover} alt=""/>
                 </div> 
-                <div className="overlay">
+                <motion.div className="overlay" initial={{ opacity: 0 }} animate={{ opacity: 1}}
+                transition={{ delay: 0.7}}>
                     <div className="title">
                         <h3> {title} </h3>
                     </div>
@@ -23,8 +26,8 @@ const MovieComponent = ({ id, title, rating, release, overview, cover_path}) => 
                     <div className="overview">
                         <p> {overview} </p>
                     </div>
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
         </Link>
      );
 }
--- src/components/SerieComponent.js
+++ src/components/SerieComponent.js
@@ -2,20 +2,23 @@ import React from 'react'
 import { fetchCover } from "../api/combinedAPI"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
 const SerieComponent = ({id, title, cover_path, rating, release, overview, popularity}) => {
     const cover = fetchCover(cover_path);
     return ( 
         <Link style={{ textDecoration: "none" }} to={{ pathname: `/serie/${id}` }}>
-            <div className="serie">
+            <motion.div initial={{ opacity: 0, scale: 0}} animate={{ opacity: 1, scale: 1 }} 
+                transition={{ duration: 0.7 }} className="serie">
                 <div className="serie-image">
                     <img src={cover} alt=""/>
                 </div>
                 <div className="etiquette">
                      TV
                 </div> 
-                <div className="overlay">
+                <motion.div className="overlay" initial={{ opacity: 0 }} animate={{ opacity: 1}}
+                transition={{ delay: 0.7}}>
                     <div className="title">
                         <h3>{title}</h3>
                     </div>
@@ -26,8 +29,8 @@ const SerieComponent = ({id, title, cover_path, rating, release, overview, popul
                     <div className="overview">
                         <p> {overview} </p>
                     </div>
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
         </Link>
      );
 }
